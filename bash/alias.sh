725f272c6d9226cee8ad097fbe30d6432c18ac5a
Cameron Brouk <cameronbrouk@gmail.com>
2021-02-04 14:04:32 -0600

    Add glines function
    tracks the amount of lines an author has added in a branch over time

--- bash/alias.sh
+++ bash/alias.sh
@@ -2,7 +2,6 @@
 alias ls="ls -a --sort=extension --width=80 --color=auto"
 alias ll='ls -alF'
 alias la='ls -A'
-alias l='ls -CF'
 
 # Faster git typing
 alias g="git"
@@ -11,5 +10,8 @@ alias g="git"
 alias vi="nvim"
 alias vim="nvim"
 
-
-
+function glines {
+  read -p "Enter Date to start: " date
+  git log --author=$1 --since=$date --pretty=tformat: --numstat \
+  | gawk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "ADDED: %s | REMOVED: %s | TOTAL ADDED: %s\n", add, subs, loc }' -
+}
