c4dde78557793410e72f6713f6330a5a26e6fc96
Cameron Brouk <cameronbrouk@gmail.com>
2021-02-04 14:08:22 -0600

    add exports to easily move/change files in Windows

--- bash/exports.sh
+++ bash/exports.sh
@@ -4,4 +4,17 @@ export VIMDATA=~/.local/share/nvim
 
 export PATH=$PATH:$VIMCONFIG/pack/bundle/start/fzf/bin
 
+export DOWNLOADS=/mnt/c/Users/Camer/Downloads
+export DESKTOP=/mnt/c/Users/Camer/Desktop
+export DOCUMENTS=/mnt/c/Users/Camer/Documents
+export PICTURES=/mnt/c/Users/Camer/Pictures
+
+# export PYENV_ROOT="$HOME/.pyenv" 
+# export PATH="$PYENV_ROOT/bin:$PATH" 
+
+# if command -v pyenv 1>/dev/null 2>&1
+# then  
+#   eval "$(pyenv init -)"\n 
+# fi
+
 
