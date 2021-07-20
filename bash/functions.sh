4f064f3183003f5780330d260bed54eeda9b1ab5
Cameron Brouk <cameronbrouk@gmail.com>
2021-07-20 12:20:26 -0500

    add function for finding all node_modules folders

--- bash/functions.sh
+++ bash/functions.sh
@@ -3,3 +3,7 @@ function glines {
   git log --author=$1 --since=$date --pretty=tformat: --numstat \
   | gawk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "ADDED: %s | REMOVED: %s | TOTAL ADDED: %s\n", add, subs, loc }' -
 }
+
+function findNodeModules {
+  find . -name "node_modules" -type d -prune -print | xargs du -chs
+}
